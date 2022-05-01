de16e13bde9ce888fe7b7ae4ad6efa43b94582da
rahman872 <94477815+rahman872@users.noreply.github.com>
2022-05-01 23:04:18 +0600

    Add files via upload

---
+++ Lab task 8/contact.php
@@ -0,0 +1,63 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact_style.css">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container-heading">
+            <h1 id="contact">Contact Us</h1>
+        </div>
+
+        <div class="flex-container" style="border-bottom: 0;
+                                           border-top-left-radius: 10px;
+                                           border-top-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Corporate Headquarters</h1>
+                <p id="sub-contact">
+                    Corporate HQ<br>Easy Bank<br>
+                    1985 Cedar Bridge Ave, Suite 3<br>
+                    Lakewood, NY 08701
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">General Contact</h1>
+                <p id="sub-contact">
+                    Corporate HQ<br>Easy Bank<br>
+                    1985 Cedar Bridge Ave, Suite 3<br>
+                    Dhaka, Bangladesh
+                </p>
+            </div>
+        </div>
+
+        <div class="flex-container" style="border-top: 0;
+                                           border-bottom-left-radius: 10px;
+                                           border-bottom-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Customer Care (24x7)</h1>
+                <p id="sub-contact">
+                  Corporate HQ<br>Easy Bank<br>
+                    1985 Cedar Bridge Ave, Suite 3<br>
+                    Dhaka, Bangladesh
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">Live Chat</h1>
+                <p id="sub-contact">
+                 Corporate HQ<br>Easy Bank<br>
+                    1985 Cedar Bridge Ave, Suite 3<br>
+                    Dhaka, Bangladesh
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
